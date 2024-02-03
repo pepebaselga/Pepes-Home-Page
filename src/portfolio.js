af048d9436c44bea4d21801c7b6a19fe351b614e
Pepe Baselga-Garriga <jbb294@cornell.edu>
2024-02-03 16:17:55 -0500

    react

---
+++ src/portfolio.js
@@ -0,0 +1,20 @@
+import emoji from "react-easy-emoji";
+const greeting = {
+  username: "Saad Pasta",
+  title: "Hi all, I'm Saad",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/saadpasta",
+  linkedin: "https://www.linkedin.com/in/saadpasta/",
+  gmail: "saadpasta70@gmail.com",
+  gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/saad.pasta7"
+};
