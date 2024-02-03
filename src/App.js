e8b01f9ccbdafc70987e5b78fa8e41157d5c987c
Pepe Baselga-Garriga <jbb294@cornell.edu>
2024-02-03 15:44:52 -0500

    react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Main from "./containers/Main";
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+export default App;
